32df92b10f406fe376cddd29e8345c0429114aba
thelichenhao <1700012738@pku.edu.cn>
2023-03-21 17:20:02 -0400

    Add register page

--- HelloWorld/login/urls.py
+++ HelloWorld/login/urls.py
@@ -1,7 +1,8 @@
 # urls.py
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, RegisterView
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- HelloWorld/login/views.py
+++ HelloWorld/login/views.py
@@ -1,4 +1,5 @@
 # views.py
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.views import View
@@ -16,7 +17,22 @@ class LoginView(View):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home') # replace 'home' with the name of your home page
+            return redirect('/') # replace 'home' with the name of your home page
         else:
             error_msg = 'Invalid login credentials'
             return render(request, self.template_name, {'error_msg': error_msg})
+
+class RegisterView(View):
+    template_name = 'login/register.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        return redirect('/login')
+
+    
