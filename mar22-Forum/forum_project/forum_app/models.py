9c4775b61d68ae4a4dd761d5ed69cc8ea1a7a1de
EMMA-JX <xiangjilucky@gmail.com>
2023-03-30 15:15:23 -0400

    draft-main page

---
+++ mar22-Forum/forum_project/forum_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Thread(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    region = models.CharField(max_length=100, default='Unknown')  # Add default value
+    interest = models.CharField(max_length=100, default='General')  # Add default value
+
+
+    def __str__(self):
+        return self.title
