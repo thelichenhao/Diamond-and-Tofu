f60b0753bd6848f37cd0dbd7a18d40b58d92ca4e
thelichenhao <1700012738@pku.edu.cn>
2023-04-03 23:57:48 -0400

    Update .gitignore

--- forum_project/forum_project/settings.py
+++ forum_project/forum_project/settings.py
@@ -57,8 +57,8 @@ ROOT_URLCONF = 'forum_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'forum_app/templates')],
-        'APP_DIRS': False,
+        'DIRS': [],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
