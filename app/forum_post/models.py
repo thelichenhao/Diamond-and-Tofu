0f99bc2dd95eafdee028fe815a5cc4ef5a571f69
thelichenhao <1700012738@pku.edu.cn>
2023-04-13 23:39:38 -0400

    Add Gunicorn

--- app/forum_post/models.py
+++ app/forum_post/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 # Create your models here.
 # Class: ForumPost
 class ForumPost(models.Model):
@@ -19,6 +20,6 @@ class ForumPost(models.Model):
     # Methods
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
