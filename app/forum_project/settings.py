a705d67619a7fe370bd295c88cc214cfd8066421
thelichenhao <1700012738@pku.edu.cn>
2023-04-14 00:27:16 -0400

    Add nginx and move static files

--- app/forum_project/settings.py
+++ app/forum_project/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = ''
+STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = ['static', ]
+# STATICFILES_DIRS = ['static', ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
